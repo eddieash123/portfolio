a91148c9f2d52a078a29ae9c410322f880f0d070
eddieash123 <53581186+eddieash123@users.noreply.github.com>
2024-03-18 10:40:31 -0400

    project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AnimatedBackgroundComponent } from './animated-background/animated-background.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AnimatedBackgroundComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,50 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  header_variable=false;
+  collapseVar = true
+
+  constructor() { }
+  
+  ngOnInit() { }
+  
+  @HostListener("document:scroll") 
+  scrollFunction() {
+    if((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)) {
+      this.header_variable=true;
+      return true;
+    }
+    else if(!this.collapseVar) {
+      this.header_variable=true;
+      return true;
+    }
+    else {
+      this.header_variable=false;
+      return false;
+    }
+  }
+  
+  menuClick() {
+    
+    if(this.collapseVar && !this.scrollFunction()){
+      this.header_variable=true
+    }
+    else if(!this.collapseVar && !this.scrollFunction()) {
+      this.header_variable=false;
+    }
+    else if(!this.collapseVar && this.scrollFunction()) {
+      this.header_variable = true;
+    }
+    this.collapseVar = !this.collapseVar;
+    
+  }
+
+  toPage(name: string) {
+    document.getElementById(name)?.scrollIntoView();
+  }
+}
