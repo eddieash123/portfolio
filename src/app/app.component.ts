0de8b276dd2fbe4cb74af4e66a233fa853b175c2
eddieash123 <53581186+eddieash123@users.noreply.github.com>
2024-04-16 10:20:28 -0400

    routing to the top fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        window.scrollTo({ top: 0, behavior: 'auto' }); // Scroll to the top instantaneously
+      }
+    });
+  }
 }
